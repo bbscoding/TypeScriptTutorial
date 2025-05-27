0d234518882bb156335e179a74d072c0851875da
bbscoding <batuhan5534@gmail.com>
2025-05-27 08:11:46 +0300

    Lesson2

---
+++ lesson2/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "John Doe";
+let meaningOfLife;
+let isLoaded;
+let album;
+myName = "Jane Smith";
+meaningOfLife = 42;
+isLoaded = true;
+album = "Van Halen";
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson2/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = "John Doe";
+let meaningOfLife: number
+let isLoaded: boolean
+let album: string | number;
+
+
+myName = "Jane Smith";
+meaningOfLife = 42;
+isLoaded = true;
+album = "Van Halen"
+
+const sum = (a: number, b:string) => {
+    return a + b;
+}
+
+let postId: string | number 
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g;
