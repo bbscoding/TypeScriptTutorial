77cc11f0be78666be9f529f103bb3da5160e0349
bbscoding <batuhan5534@gmail.com>
2025-06-10 08:22:16 +0300

    React Hooks with Typescript-lesson13

---
+++ lesson13/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useCallback, useMemo, useRef, type MouseEvent, type KeyboardEvent } from 'react'
+import './App.css'
+
+interface User {
+  id: number,
+  username: string
+}
+
+type fibFunc = (n: number) => number
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n
+  return fib(n - 1) + fib(n - 2)
+}
+
+const myNum: number = 37
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+  const [users, setUsers] = useState<User[] | null>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  console.log(inputRef?.current)
+  console.log(inputRef?.current?.value)
+
+  useEffect(() => {
+    console.log('mounting')
+    console.log("Users: ", users)
+  }, [users])
+
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>) => setCount(prev => prev + 2), [])
+
+  const result = useMemo<number>(() => fib(myNum), [myNum])
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text" />
+    </>
+  )
+}
+
+export default App
